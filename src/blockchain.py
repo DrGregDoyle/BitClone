287fdcd54ba5a59959410cb0aecbc1ae61a5c573
Dr. Greg Doyle <drgregdoyle@gmail.com>
2024-06-26 12:11:53 -0400

    Skeleton for blockchain

--- src/blockchain.py
+++ src/blockchain.py
@@ -2,18 +2,43 @@
 The Blockchain class
 """
 
+import logging
 # --- IMPORTS --- #
+import sys
+
 from src.block import Block
+from src.database import Database
+from src.encoder_lib import bits_to_target
+
+MAX_TARGET = 0x00000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+
+log_level = logging.DEBUG
+logger = logging.getLogger(__name__)
+logger.setLevel(log_level)
+logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 
 
 # --- CLASSES --- #
 class Blockchain:
-    MAX_TARGET = 0x00000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
 
     def __init__(self):
         self.chain = []
-        self.target = 0
+        self.height = 0
+        self.utxos = Database()
 
     def add_block(self, candidate_block: Block):
-        header = candidate_block.header
-        tx_list = candidate_block.tx_list
+        # Validate block
+        block_validated = self.validate_block(candidate_block)
+        # Add block
+        self.chain.append(candidate_block)
+
+        # Update UTXOs / Process Transactions
+
+        # Return True/False
+
+    def validate_block(self, _block: Block):
+        # Verify block_id is smaller than target
+        block_target = bits_to_target(_block.bits)
+        if int(block_target, 16) < int(_block.id, 16):
+            logger.error("Block ID larger than target")
+            return False
