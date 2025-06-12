8baa573c0ff69c411f87f7f2618c6c3af3e27e86
Dr. Greg Doyle <drgregdoyle@gmail.com>
2025-06-12 18:55:41 -0400

    Working on Messaging

--- src/network/__init__.py
+++ src/network/__init__.py
@@ -2,4 +2,5 @@
 network folder used to house all files dealing with networking functions
 """
 # network/__init__.py
-from src.network.comms import Header, Version, decode_message
+from src.network.comms import Header, decode_message
+from src.network.msg_type import Version
