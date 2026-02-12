27da99899074b51e2c25cfd678463180db49b9a1
Greg Doyle <drgregdoyle@gmail.com>
2026-02-11 19:42:25 -0500

    Working on MemPool

--- src/mempool/mempool.py
+++ src/mempool/mempool.py
@@ -4,10 +4,14 @@ The MemPool class
 from pathlib import Path
 
 from src.core import ReadError, get_logger, TransactionError
-from src.database.database import BitCloneDatabase, DB_PATH
+from src.database.database import BitCloneDatabase
+from src.script import ScriptEngine
 from src.tx import Transaction
 
 logger = get_logger(__name__)
+
+# --- TEST DB PATH FOR DEVELOPMENT --- #
+TEST_DB_PATH = Path(__file__).parent / "db_files" / "test_db.sqlite3"
 
 
 class MemPoolTx:
@@ -30,7 +34,7 @@ class MemPool:
     MAX_TIME = 3600 * 336  # 336 hours = 2 weeks
     MIN_FEE = 1  # sats/vbyte = feerate
 
-    def __init__(self, db_path: Path = DB_PATH) -> None:
+    def __init__(self, db_path: Path = TEST_DB_PATH) -> None:
         # --- MemPool constants
         self.max_size = MemPool.MAX_SIZE
         self.max_time = MemPool.MAX_TIME
@@ -38,6 +42,9 @@ class MemPool:
 
         # --- UTXO set
         self.btcdb = BitCloneDatabase(db_path)
+
+        # --- Script Engine for Tx Validation
+        self.script_engine = ScriptEngine()
 
         # --- MemPool storage
         self.mempool = {}  # Dict where the key will be the txid
@@ -57,8 +64,24 @@ class MemPool:
             logger.error("Failed to validate tx")
             return False
 
+        # --- Find ancestors
+        # --- If a Tx has an ancestor, that means the input references one of the outputs of the ancestor. Hence for
+        # each input, we want to strip away the txid from its outpoint and look for that tx in the mempool
+        ancestors = []
+        for txin in tx.inputs:
+            temp_id = txin.outpoint[:32]  # outpoint = txid (32 bytes) + vout (4 bytes)
+            if temp_id in self.mempool.keys():
+                ancestors.append(self.mempool[temp_id])
+
+        # --- Create MemPoolTx
+        mempool_tx = MemPoolTx(
+            tx=tx,
+            fee=self._get_fee(tx),
+            ancestors=ancestors,
+        )
+
         # --- Add tx
-        self.mempool.update({tx.txid: tx})
+        self.mempool.update({tx.txid: mempool_tx})
         return True
 
     def _validate_tx(self, tx: Transaction) -> bool:
@@ -73,16 +96,13 @@ class MemPool:
             return False
 
         # --- Check fees
-        utxos = self._get_utxos(tx)
-        input_total = 0
-        for utxo in utxos:
-            input_total += utxo.amount
-        output_total = 0
-        for output in tx.outputs:
-            output_total += output.amount
-        tx_fee = input_total - output_total
-        if tx_fee < 0:
-            raise TransactionError("Output total exceeds input total")
+        try:
+            tx_fee = self._get_fee(tx)
+        except TransactionError as e:
+            logger.error(f"Validation error: {e}")
+            return False
+
+        # --- Validate Scripts
 
         return True
 
@@ -97,3 +117,26 @@ class MemPool:
                 raise ReadError(f"Failed to find utxo with outpoint {txin.outpoint}")
             utxos.append(temp_utxo)
         return utxos
+
+    def _get_fee(self, tx: Transaction) -> int:
+        """
+        We return the tx fee amount in sats
+        """
+        # setup
+        utxos = self._get_utxos(tx)
+        input_total = 0
+        output_total = 0
+        for utxo in utxos:
+            input_total += utxo.amount
+        for output in tx.outputs:
+            output_total += output.amount
+        tx_fee = input_total - output_total
+        if tx_fee < 0:
+            raise TransactionError("Output total exceeds input total")
+        return tx_fee
+
+
+# ---TESING --- #
+if __name__ == "__main__":
+    # testing db capabilities
+    test_mempool = MemPool()
